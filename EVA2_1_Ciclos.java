e027da17ad3de7bcbeaa390147812583fb97b107
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-10-07 11:37:35 -0600

    Add files via upload

---
+++ EVA2_1_Ciclos.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_1_Ciclos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //repericion de intrucciones:
+        // ciclos o Blucles
+        // for (desde) Cuando saben la cantidad de veces que se va a repetir la instruccion
+                     // cuando saben cuantas veces se va a repetir la intrucccion es un contador 
+                             // tiene 3 parametros : punto de inicio, condicion, avance o retroceso
+        // While (mientras) cuando se repite cero(0) o N veces
+        // Do While (Hacer...Mientras) cuando se repite al menos 1 vez hasta n veces
+        
+        // repeticion de intrucciones:
+        // Ciclos o Bucles
+        // Ciclo FOR
+
+
+        for(int i = 1; i <= 10; i++){// ++ --> operador para sukmar 1 Unidad
+            // -- --> resta una unidad  //+= Suma N valor   // -= resta N valor  
+            //*= multiplica por N  // /= divide entre N 
+            System.out.println(i + " - "); // la variable i solo existe dentro del ciclo FOR
+            }
+                System.out.println("");
+        for(int i = 10; i >= 1; i--){// ++ --> operador para sukmar 1 Unidad
+            // -- --> resta una unidad  //+= Suma N valor   // -= resta N valor  
+            //*= multiplica por N  // /= divide entre N 
+            System.out.println(i + " - ");  
+        }
+        
+    }
+    
+}
