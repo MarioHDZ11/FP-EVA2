56723a8824a9ad9105d736b1ad257e25e08f6b0f
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-10-10 11:38:09 -0600

    Add files via upload

---
+++ EVA2_3_Potencia.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_3_potencia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_3_Potencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int base, expo;
+        Scanner cap = new Scanner(System.in);
+        
+        System.out.println("Introduce el numero:");
+        base = cap.nextInt();
+        System.out.println("Introduce la potencia:");
+        expo = cap.nextInt();
+
+        int resu = 0;        
+        for(int i =1; i <= expo; i++){
+            resu = resu * expo;
+    }
+        System.out.println("El resultado es:" + resu);
+    }
+    
+}
