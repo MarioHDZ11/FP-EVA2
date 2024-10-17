6788a86eabed4dd0d48b57bee5dd636d82d440a7
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-10-17 09:50:09 -0600

    Add files via upload

---
+++ Eva2_5_Break_Continue.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_5_break_continue;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva2_5_Break_Continue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Break --> termina el ciclo completamente
+        //Continue --> Interrumpe la repeticion actual
+        
+        for(int i = 1; i <= 10; i++){
+            if(i == 8)
+                break;
+            System.out.print(i + " - ");
+        }
+        System.out.println("");
+                for(int i = 1; i <= 10; i++){
+            if(i == 8)
+                continue;
+            System.out.print(i + " - ");
+        }
+        
+    }
+    
+}
