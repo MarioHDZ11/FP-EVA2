46486d879e85d404b7ff59524bf7df6e0d03aec6
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-10-21 13:49:19 -0600

    Add files via upload

---
+++ EVA2_13_Arreglos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_13_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_13_Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // int[] --> Indica que es un arreglo de enteros
+        // new int[10] -->crea el arreglo con 10 espacios para enteros
+        int[] arreglo = new int[10]; 
+        arreglo[0] = 100;// ponemos 100 en la posicion cero
+        arreglo[9] = 900;// Ultima posicion (10 - 1)
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+    }
+    
+}
