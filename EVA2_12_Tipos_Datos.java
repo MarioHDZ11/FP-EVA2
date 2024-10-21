44bf6a5b722dd2d2169ef0919bda45d013d5973d
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-10-21 13:28:09 -0600

    Add files via upload

---
+++ EVA2_12_Tipos_Datos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_tipos_datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_Tipos_Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte valor = -128;
+        byte valor2 = 127;
+        System.out.println("valor:" + (valor));
+        System.out.println("valor2:" + (valor2));
+        short val = -32768;
+        short val2 = 32767;
+        int x = -2147483618;
+        int y = 2147483617;
+        //long a = -922337203685475808;
+        //long b = 922337203685475807;
+    }
+    
+}
